81c7284bbf460b41166df7cc4a5c152372023b53
thais maciel <thaismaciielf@hotmail.com>
2024-02-29 20:36:02 -0300

    add new file teste

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 // CRIAR FUNCTION QUE MUDE PARA UMA VERSÃO DARK O PORTFOLIO E QUE TRADUZA PARA INGLES //
 // download do meu curriculo //
+
+let dark__mode = document.getElementById('dark__mode');
+let body = document.querySelector('body');
+
+dark__mode.addEventListener('click', ()=> {
+    dark__mode.classList.toggle('dark');
+    body.classList.toggle('dark');
+})
